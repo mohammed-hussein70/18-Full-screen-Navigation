a5e539f4b5da6536c486a7cf3556fcad047c64fc
mohammed <mohammedhussein004@gmail.com>
2024-10-20 16:54:05 +0300

    add

--- main.js
+++ main.js
@@ -2,18 +2,14 @@ function animation() {
   let openBtn = document.querySelector("button");
   let links = document.querySelector(".links");
   let closeBtn = document.querySelector(".close");
-  // hide button close
-  closeBtn.style.display = "none";
   // click button open
   openBtn.addEventListener("click", function () {
     links.style.width = "100%";
-    openBtn.style.display = "none";
     closeBtn.style.display = "block";
   });
   // click button close
   closeBtn.addEventListener("click", function () {
     links.style.width = "0";
-    closeBtn.style.display = "none";
     openBtn.style.display = "block";
   });
 }
